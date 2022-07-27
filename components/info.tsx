38aa666288a0c751bd07d0dcccdfcdf044744ad3
NickP-real <nick_narak@rocketmail.com>
2022-07-28 01:03:03 +0700

    feat(home page): ui drafting

---
+++ components/info.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Info: React.FC = () => {
+  return <div className="flex-grow bg-white rounded-[40px]">tt</div>;
+};
+
+export default Info;
---
+++ components/menu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu: React.FC = () => {
+  return (
+    <div className="flex justify-center justify-self-center items-center text-black bg-white sm:max-w-none aspect-square drop-shadow-1xl rounded-[40px] max-w-[10rem]">
+      Menu
+    </div>
+  );
+};
+
+export default Menu;
