cf8f269aee3f07723bcd59e88d5d00e6f8f91093
NickP-real <nick_narak@rocketmail.com>
2022-09-13 13:00:48 +0700

    feat(navbar): linking

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -1,35 +1,59 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-const AdminNavbar: React.FC = () => {
+interface NavbarData {
+  value: string;
+  href: string;
+}
+
+const NavbarLayout: React.FC<NavbarData[]> = (datas: NavbarData[]) => {
   return (
     <>
       <ul className="flex flex-grow gap-x-1 md:gap-x-8">
-        <li>จัดการหลักสูตร</li>
-        <li>จัดการข้อมูลแผนการศึกษา</li>
-        <li>จัดการข้อมูลรายวิชาที่เปิดสอน</li>
+        {datas.map((data) => {
+          return (
+            <Link href={data.href} key={data.value}>
+              <a>{data.value}</a>
+            </Link>
+          );
+        })}
       </ul>
       <LogoutAndProfile />
     </>
   );
 };
 
+const AdminNavbar: React.FC = () => {
+  const adminNav: NavbarData[] = [
+    { value: "จัดการหลักสูตร", href: "/admin/" },
+    { value: "จัดการข้อมูลแผนการศึกษา", href: "/admin/" },
+    { value: "จัดการข้อมูลรายวิชาที่เปิดสอน", href: "/admin/" },
+  ];
+  return NavbarLayout(adminNav);
+};
+
 const StudentNavbar: React.FC = () => {
-  return (
-    <>
-      <ul className="flex flex-grow gap-x-8">
-        <li>จัดการข้อมูลการเรียน</li>
-        <li>แนะนำแผนการศึกษา</li>
-      </ul>
-      <LogoutAndProfile />
-    </>
-  );
+  const studentNav: NavbarData[] = [
+    { value: "จัดการข้อมูลการเรียน", href: "/main/student/manage_course" },
+    { value: "แนะนำแผนการศึกษา", href: "/main/student/" },
+  ];
+  return NavbarLayout(studentNav);
 };
 
 const LogoutAndProfile: React.FC = () => {
   return (
-    <div className="flex gap-x-3">
-      <Image alt="profile" src="/profile.svg" width={69 / 2} height={69 / 2} />
+    <div className="flex items-center gap-x-3">
+      <Link href="/main/student/profile" passHref>
+        <a className="flex items-center justify-center">
+          <Image
+            alt="profile"
+            src="/profile.svg"
+            width={69 / 2}
+            height={69 / 2}
+          />
+        </a>
+      </Link>
       <h3>ออกจากระบบ</h3>
     </div>
   );
@@ -48,9 +72,12 @@ export const Navbar: React.FC<NavbarType> = ({ type }: NavbarType) => {
     }
   }
   return (
-    <div className="fast-text container mx-auto flex h-full items-center justify-between text-sm md:text-2xl">
-      {Navtype(type)}
-    </div>
+    <header className="relative h-16 bg-white">
+      <div className="fast-text container mx-auto flex h-full items-center justify-between text-sm md:text-2xl">
+        {Navtype(type)}
+      </div>
+      <div className="container mx-auto h-2 bg-gradient-to-r from-fgreen to-fpurple"></div>
+    </header>
   );
 };
 
