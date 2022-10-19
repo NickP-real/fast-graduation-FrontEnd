2cf42deac3ce563392016aa6fa05f66411850f41
NickP-real <nick_narak@rocketmail.com>
2022-10-20 05:53:23 +0700

    also same as previous

--- components/modal/description.tsx
+++ components/modal/description.tsx
@@ -1,14 +1,14 @@
 import Modal from "./modal";
 
-const DescriptionModal: React.FC = () => {
-  return (
-    <Modal title="รายละเอียดวิชา : Description">
-      <div>
-        <span>รหัสวิชา</span>
-        <span></span>
-      </div>
-    </Modal>
-  );
-};
+// const DescriptionModal: React.FC = () => {
+//   return (
+//     <Modal title="รายละเอียดวิชา : Description">
+//       <div>
+//         <span>รหัสวิชา</span>
+//         <span></span>
+//       </div>
+//     </Modal>
+//   );
+// };
 
 // export default DescriptionModal;
